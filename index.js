1fdd301edf5ed3cc5b66f4d064adb9b2c340ae4d
Ahsan Habib Nahid <ahsanhabib3239@gmail.com>
2021-10-30 10:57:43 +0600

    cors / middleware add

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('i know node js')
+    const data = {
+        name: 'Nahid',
+        age: 25,
+        city: 'Dhaka'
+    }
+    res.send(data)
+})
+
+const users = ['nahid', 'zahid', 'mridul','forhad']
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id
+    const name = users[id]
+    const obj = {id, name}
+
+    console.log(req.query)
+
+    res.send(obj)
 })
 
 
